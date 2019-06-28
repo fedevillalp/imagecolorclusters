d736a6040fe1b2d147d4635389466a8c90024c4a
Federico <federico@Federicos-MacBook-Pro.local>
2019-06-28 13:57:10 -0700

    first commit

--- temp.py
+++ temp.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Federico Villalapndo 6/28/2019
+by Federico Villalapndo 6/28/2019
+
+Reference: 
+https://www.dataquest.io/blog/tutorial-colors-image-clustering-python/
 """
 
 import matplotlib.pyplot as plt
@@ -12,12 +15,11 @@ from scipy.cluster.vq import whiten
 from scipy.cluster.vq import kmeans
 import numpy as np
 
-
-#image = plt.imread('./dataquest.jpg')
+# OPEN IMAGE FILE
 image = plt.imread('./test3.jpg')
 plt.imshow(image)
 
-
+# GET THE IMAGE R, G, B VALUES
 r = []
 g = []
 b = []
@@ -29,9 +31,7 @@ for line in image:
         g.append(temp_g)
         b.append(temp_b)
 
-#print(len(r), len(g), len(b))
-
-
+# PLOT THE IMAGE PIXELS IN R, G, B,  0-255 SPACE
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.scatter(r, g, b)
@@ -39,33 +39,37 @@ ax.set_xlabel('R')
 ax.set_ylabel('G')
 ax.set_zlabel('B')
 
+# CREATE PANDA DATA FRAME
 d = {'r': r, 'g': g,'b': b}
 pandaDataFrame = pd.DataFrame(data=d)
 
-
+# STANDARDIZE R,G,B PIXEL DATA
 pandaDataFrame['scaled_red'] = whiten(pandaDataFrame['r'])
 pandaDataFrame['scaled_green'] = whiten(pandaDataFrame['g'])
 pandaDataFrame['scaled_blue'] = whiten(pandaDataFrame['b'])
-
 #print(pandaDataFrame.sample(n = 5))
 
+# PERFORM K-MEANS CLUSTERING ON IMAGE R,G,B PIXEL INFORMATION
 x = kmeans(pandaDataFrame[['scaled_red', 'scaled_green', 'scaled_blue']], 6)
 cluster_centers = x[0]
 print("CLUSTER CENTERS:",cluster_centers)
 
+# ADD STANDARDIZED INFORMATION TO DATAFRAME
 r_std, g_std, b_std = pandaDataFrame[['r', 'g', 'b']].std()
-#
 colors = []
 
+# GET COLOR CENTERS, MULTIPLY BY STD TO BRING BACK TO 0-255 AND NORMALIZE TO GO INTO 0-1 RANGE
 for cluster_center in cluster_centers:
     scaled_r, scaled_g, scaled_b = cluster_center
     #print(cluster_center)
     colors.append((scaled_r * r_std / 255, scaled_g * g_std / 255, scaled_b * b_std / 255))
 
+#PLOT THE COLORS RESULTING FROM K-MEANS CLUSTERING
 print("COLORS:",colors)
-
 plt.figure()
 plt.imshow([colors])
 plt.show()
+
+
 
 
